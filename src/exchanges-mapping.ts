7d7a23e2e12645e5376751244f4a0b5ceb4996e5
justin j. moses <justinjmoses@gmail.com>
2019-10-06 10:33:49 -0400

    Removing older way to load fee data

--- src/exchanges-mapping.ts
+++ src/exchanges-mapping.ts
@@ -1,8 +1,4 @@
 import { Synthetix, SynthExchange as SynthExchangeEvent } from '../generated/Synthetix/Synthetix';
-import { Synthetix as SynthetixForXDR4 } from '../generated/SynthXDR4/Synthetix';
-import { Synth as SynthXDR4, Issued } from '../generated/SynthXDR4/Synth';
-import { Synthetix as SynthetixForXDR32 } from '../generated/SynthXDR32/Synthetix';
-import { Synth as SynthXDR32 } from '../generated/SynthXDR32/Synth';
 
 import { Total, SynthExchange, Exchanger } from '../generated/schema';
 
@@ -10,7 +6,7 @@ import { BigInt, Address } from '@graphprotocol/graph-ts';
 
 import { exchangesToIgnore } from './exchangesToIgnore';
 
-import { attemptEffectiveValue, sUSD32, sUSD4 } from './common';
+import { attemptEffectiveValue } from './common';
 
 function getMetadata(): Total {
   let total = Total.load('1');
@@ -92,39 +88,4 @@ export function handleSynthExchange4(event: SynthExchangeEvent): void {
 
 export function handleSynthExchange32(event: SynthExchangeEvent): void {
   handleSynthExchange(event, true);
-}
-
-// Issuing of XDR is our fee mechanism
-function handleIssuedXDR(event: Issued, useBytes32: boolean): void {
-  if (exchangesToIgnore.indexOf(event.transaction.hash.toHex()) >= 0) {
-    return;
-  }
-
-  if (useBytes32) {
-    let synthXDR = SynthXDR32.bind(event.address);
-    let synthetix = SynthetixForXDR32.bind(synthXDR.synthetixProxy());
-    let effectiveValueTry = synthetix.try_effectiveValue(synthXDR.currencyKey(), event.params.value, sUSD32);
-    if (!effectiveValueTry.reverted) {
-      let metadata = getMetadata();
-      metadata.totalFeesGeneratedInUSD_old = metadata.totalFeesGeneratedInUSD.plus(effectiveValueTry.value);
-      metadata.save();
-    }
-  } else {
-    // This duping isn't great. We could probably rework it using synthXDR and synthetix as SmartContract and invoke call / tryCall
-    let synthXDR = SynthXDR4.bind(event.address);
-    let synthetix = SynthetixForXDR4.bind(synthXDR.synthetix());
-    let effectiveValueTry = synthetix.try_effectiveValue(synthXDR.currencyKey(), event.params.value, sUSD4);
-    if (!effectiveValueTry.reverted) {
-      let metadata = getMetadata();
-      metadata.totalFeesGeneratedInUSD_old = metadata.totalFeesGeneratedInUSD.plus(effectiveValueTry.value);
-      metadata.save();
-    }
-  }
-}
-
-export function handleIssuedXDR32(event: Issued): void {
-  handleIssuedXDR(event, true);
-}
-export function handleIssuedXDR4(event: Issued): void {
-  handleIssuedXDR(event, false);
 }
