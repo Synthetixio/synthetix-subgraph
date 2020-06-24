a5547e4f08ef44ab39335fda94ee992822727ab2
Clement Balestrat <clement.balestrat@gmail.com>
2020-06-24 13:12:25 +1000

    PR fixes

--- src/loans-mapping.ts
+++ src/loans-mapping.ts
@@ -6,9 +6,8 @@ import {
 import { Loan, LoanCreated, LoanClosed } from '../generated/schema';
 
 export function handleLoanCreated(event: LoanCreatedEvent): void {
-  let loanId = event.params.loanID;
-  let loanEntity = new Loan(loanId.toHex());
-  let loanCreatedEntity = new LoanCreated(loanId.toHex());
+  let loanEntity = new Loan(event.params.loanID.toHex());
+  let loanCreatedEntity = new LoanCreated(event.transaction.hash.toHex() + '-' + event.logIndex.toString());
 
   loanEntity.acount = event.params.account;
   loanEntity.amount = event.params.amount;
@@ -17,18 +16,19 @@ export function handleLoanCreated(event: LoanCreatedEvent): void {
 
   loanCreatedEntity.account = event.params.account;
   loanCreatedEntity.amount = event.params.amount;
+  loanCreatedEntity.loanId = event.params.loanID;
   loanCreatedEntity.save();
 }
 
 export function handleLoanClosed(event: LoanClosedEvent): void {
-  let loanId = event.params.loanID;
-  let loanEntity = Loan.load(loanId.toHex());
-  let loanClosedEntity = new LoanClosed(loanId.toHex());
+  let loanEntity = Loan.load(event.params.loanID.toHex());
+  let loanClosedEntity = new LoanClosed(event.transaction.hash.toHex() + '-' + event.logIndex.toString());
 
   loanEntity.isOpen = false;
   loanEntity.save();
 
   loanClosedEntity.account = event.params.account;
+  loanClosedEntity.loanId = event.params.loanID;
   loanClosedEntity.feesPaid = event.params.feesPaid;
   loanClosedEntity.save();
 }
