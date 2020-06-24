b2b96f8b84096b03cf9f38d0e433f50a4889b19a
Clément BALESTRAT <clement.balestrat@gmail.com>
2020-06-24 14:54:47 +1000

    adding synthetix-loans (#19)

---
+++ src/loans-mapping.ts
@@ -0,0 +1,38 @@
+import {
+  LoanCreated as LoanCreatedEvent,
+  LoanClosed as LoanClosedEvent,
+} from '../generated/EtherCollateral/EtherCollateral';
+
+import { Loan, LoanCreated, LoanClosed } from '../generated/schema';
+
+export function handleLoanCreated(event: LoanCreatedEvent): void {
+  let loanEntity = new Loan(event.params.loanID.toHex());
+  let loanCreatedEntity = new LoanCreated(event.transaction.hash.toHex() + '-' + event.logIndex.toString());
+
+  loanEntity.account = event.params.account;
+  loanEntity.amount = event.params.amount;
+  loanEntity.isOpen = true;
+  loanEntity.createdAt = event.block.timestamp;
+  loanEntity.save();
+
+  loanCreatedEntity.account = event.params.account;
+  loanCreatedEntity.amount = event.params.amount;
+  loanCreatedEntity.loanId = event.params.loanID;
+  loanCreatedEntity.timestamp = event.block.timestamp;
+  loanCreatedEntity.save();
+}
+
+export function handleLoanClosed(event: LoanClosedEvent): void {
+  let loanEntity = Loan.load(event.params.loanID.toHex());
+  let loanClosedEntity = new LoanClosed(event.transaction.hash.toHex() + '-' + event.logIndex.toString());
+
+  loanEntity.isOpen = false;
+  loanEntity.closedAt = event.block.timestamp;
+  loanEntity.save();
+
+  loanClosedEntity.account = event.params.account;
+  loanClosedEntity.loanId = event.params.loanID;
+  loanClosedEntity.feesPaid = event.params.feesPaid;
+  loanClosedEntity.timestamp = event.block.timestamp;
+  loanClosedEntity.save();
+}
