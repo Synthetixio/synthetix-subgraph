49e0eafab5dfd625a595261f562fd9271b774ef5
dvd-schwrtz <dbschwartz17@gmail.com>
2020-10-28 17:43:08 -0700

    fix null as a string issue

--- mustache/templates/common.js
+++ mustache/templates/common.js
@@ -1,5 +1,9 @@
 const createStartBlock = ({ prodStartBlock, testStartBlock }) =>
-  process.env.TEST_YAML ? process.env.UNIVERSAL_START_BLOCK || testStartBlock || prodStartBlock : prodStartBlock;
+  process.env.TEST_YAML
+    ? process.env.UNIVERSAL_START_BLOCK != 'null'
+      ? process.env.UNIVERSAL_START_BLOCK
+      : testStartBlock || prodStartBlock
+    : prodStartBlock;
 
 module.exports = {
   createStartBlock,
