0871f1254937dfbfaec68439b614b0394d147d06
vbstreetz <vbstreetz@gmail.com>
2021-03-26 19:56:59 +0300

    feat: no need for Account.all attribute

--- src/delegate-approvals.ts
+++ src/delegate-approvals.ts
@@ -1,3 +1,4 @@
+// import { log } from '@graphprotocol/graph-ts';
 import {
   Approval,
   WithdrawApproval,
@@ -16,7 +17,9 @@ let TRADE_ACTION = 'ExchangeForAddress';
 export function handleApproval(event: Approval): void {
   let authoriser = event.params.authoriser;
   let delegate = event.params.delegate;
-  let action = event.params.action.toHex();
+  let action = event.params.action.toString();
+
+  // log.warning('approve: {}', [action]);
 
   let id = authoriser.toHex() + '-' + delegate.toHex();
   let entity = Account.load(id);
@@ -24,21 +27,26 @@ export function handleApproval(event: Approval): void {
     entity = new Account(id);
     entity.authoriser = authoriser;
     entity.delegate = delegate;
+    entity.burn = false;
+    entity.mint = false;
+    entity.claim = false;
+    entity.exchange = false;
   }
-  if (action === ALL_ACTION) {
-    entity.all = true;
+  if (action == ALL_ACTION) {
     entity.burn = true;
     entity.mint = true;
     entity.claim = true;
     entity.exchange = true;
-  } else if (action === BURN_ACTION) {
-    entity.burn = true;
-  } else if (action === MINT_ACTION) {
-    entity.mint = true;
-  } else if (action === CLAIM_ACTION) {
-    entity.claim = true;
-  } else if (action === TRADE_ACTION) {
-    entity.exchange = true;
+  } else {
+    if (action == BURN_ACTION) {
+      entity.burn = true;
+    } else if (action == MINT_ACTION) {
+      entity.mint = true;
+    } else if (action == CLAIM_ACTION) {
+      entity.claim = true;
+    } else if (action == TRADE_ACTION) {
+      entity.exchange = true;
+    }
   }
   entity.save();
 }
@@ -46,25 +54,28 @@ export function handleApproval(event: Approval): void {
 export function handleWithdrawApproval(event: WithdrawApproval): void {
   let authoriser = event.params.authoriser;
   let delegate = event.params.delegate;
-  let action = event.params.action.toHex();
+  let action = event.params.action.toString();
+
+  // log.warning('withdraw: {}', [action]);
 
   let id = authoriser.toHex() + '-' + delegate.toHex();
   let entity = Account.load(id);
   if (entity != null) {
-    if (action === ALL_ACTION) {
-      entity.all = false;
+    if (action == ALL_ACTION) {
       entity.burn = false;
       entity.mint = false;
       entity.claim = false;
       entity.exchange = false;
-    } else if (action === BURN_ACTION) {
-      entity.burn = false;
-    } else if (action === MINT_ACTION) {
-      entity.mint = false;
-    } else if (action === CLAIM_ACTION) {
-      entity.claim = false;
-    } else if (action === TRADE_ACTION) {
-      entity.exchange = false;
+    } else {
+      if (action == BURN_ACTION) {
+        entity.burn = false;
+      } else if (action == MINT_ACTION) {
+        entity.mint = false;
+      } else if (action == CLAIM_ACTION) {
+        entity.claim = false;
+      } else if (action == TRADE_ACTION) {
+        entity.exchange = false;
+      }
     }
     entity.save();
   }
