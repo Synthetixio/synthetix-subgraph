d96251f38f5378c2622b75ed2cca9ed70b270809
David <dbschwartz17@gmail.com>
2021-04-15 13:20:59 -0700

    add hardcoded contracts from kovan ovm

--- src/hardcoded-contracts.ts
+++ src/hardcoded-contracts.ts
@@ -1,7 +1,14 @@
-export let readProxyAdressResolver = '0x1Cb059b7e74fD21665968C908806143E744D5F30';
+// export let readProxyAdressResolver = '0x1Cb059b7e74fD21665968C908806143E744D5F30';
+// export let escrowContracts = new Map<string, string>();
 
+// escrowContracts.set('escrow', '0x06c6d063896ac733673c4474e44d9268f2402a55');
+// escrowContracts.set('rewardEscrow', '0xd32138018210eda0028240638f35b70ecc0d8c22');
+// escrowContracts.set('rewardEscrowV2', '0x47ee58801c1ac44e54ff2651ae50525c5cfc66d0');
+
+// kovanOvm
+export let readProxyAdressResolver = '0x7a6f9eDDC03Db81927eA4131919343f93CA9b6a7';
 export let escrowContracts = new Map<string, string>();
 
-escrowContracts.set('escrow', '0x06c6d063896ac733673c4474e44d9268f2402a55');
-escrowContracts.set('rewardEscrow', '0xd32138018210eda0028240638f35b70ecc0d8c22');
-escrowContracts.set('rewardEscrowV2', '0x47ee58801c1ac44e54ff2651ae50525c5cfc66d0');
+escrowContracts.set('escrow', '0xf545e539788AfEAA665B7d79568E306e37Ef05cC');
+escrowContracts.set('rewardEscrow', '0x9952e42fF92149f48b3b7dee3f921A6DD106F79F');
+escrowContracts.set('rewardEscrowV2', '0xB613d148E47525478bD8A91eF7Cf2F7F63d81858');
