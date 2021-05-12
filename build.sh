3643d2e5837a455980a1d72551049bce5abbb4f3
David <dbschwartz17@gmail.com>
2021-05-12 16:37:19 -0700

    update shell scripts to build and deploy one based on passing in various inputs

--- build.sh
+++ build.sh
@@ -6,15 +6,10 @@ networks=( mainnet optimism-mainnet kovan optimism-kovan )
 if [ "all" == $network ]; then
     for i in "${networks[@]}"
     do
-	    for f in subgraphs/*.js
-        do 
-            SNX_NETWORK=$i graph codegen $f -o generated/${f%.js}
-            SNX_NETWORK=$i graph build $f -o build/$i/${f%.js}
-        done
+        SNX_NETWORK=$i graph codegen subgraphs/$subgraph.js -o generated/subgraphs/$subgraph
+        SNX_NETWORK=$i graph build subgraphs/$subgraph.js -o build/$i/subgraphs/$subgraph
     done
-elif [ "synthetix" == $subgraph ]; then
-    SNX_NETWORK=$network graph codegen subgraphs/$subgraph.js -o generated/subgraphs/$subgraph.js
-    SNX_NETWORK=$network graph build subgraphs/$subgraph.js -o build/$network/subgraphs/$subgraph.js
+else
+    SNX_NETWORK=$network graph codegen subgraphs/$subgraph.js -o generated/subgraphs/$subgraph
+    SNX_NETWORK=$network graph build subgraphs/$subgraph.js -o build/$network/subgraphs/$subgraph
 fi
-    SNX_NETWORK=$network graph codegen subgraphs/synthetix-$subgraph.js -o generated/subgraphs/synthetix-$subgraph.js
-    SNX_NETWORK=$network graph build subgraphs/synthetix-$subgraph.js -o build/$network/subgraphs/synthetix-$subgraph.js
--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,8 @@ networks=( mainnet optimism-mainnet kovan optimism-kovan )
 if [ "all" == $network ]; then
     for i in "${networks[@]}"
     do
-	    for f in subgraphs/*.js
-        do 
-            graph deploy build/$i/${f%.js}/subgraph.yaml
-        done
+        graph deploy build/$i/$subgraph/subgraph.yaml
     done
-elif [ "synthetix" == $subgraph ]; then
-    graph deploy build/$network/subgraphs/$subgraph/subgraph.yaml
+else
+    graph deploy build/$network/$subgraph/subgraph.yaml
 fi
-    graph deploy build/$network/subgraphs/synthetix-$subgraph/subgraph.yaml
