e232b459e7cb61fcb115450825e01aa4b5b27719
Noah Litvin <noah.litvin@gmail.com>
2021-11-09 16:47:57 -0500

    codegen runs successfully

---
+++ src/wrapper.ts
@@ -0,0 +1,76 @@
+import { DataSourceContext, dataSource, Address, BigDecimal } from '@graphprotocol/graph-ts';
+import { Wrapper } from '../generated/subgraphs/wrapper/schema';
+import { WrapperTemplate } from '../../generated/subgraphs/wrapper/templates';
+import { getUSDAmountFromAssetAmount, getLatestRate, strToBytes } from './lib/helpers';
+import { contracts } from '../generated/contracts';
+import { AddressResolver } from '../generated/subgraphs/wrapper/wrapper_ProxyERC20_0/AddressResolver';
+import { ethereum } from '@graphprotocol/graph-ts/chain/ethereum';
+
+function handleWrapperCreated(token: string, currencyKey: string, wrapperAddress: string): void {
+  let context = new DataSourceContext();
+  context.setString('wrapperAddress', wrapperAddress);
+
+  let wrapper = WrapperTemplate.createWithContext(wrapperAddress, context);
+  wrapper.tokenAddress = token;
+  wrapper.currencyKey = currencyKey;
+  wrapper.save();
+}
+
+function handleMinted(
+  account: Address,
+  principal: BigDecimal,
+  fee: BigDecimal,
+  amountIn: number,
+  transaction: ethereum.Transaction,
+): void {
+  let context = dataSource.context();
+  let wrapperAddress = context.getString('wrapperAddress');
+  let wrapper = Wrapper.load(wrapperAddress);
+  wrapper.amount += amountIn;
+
+  let txHash = transaction.hash.toHex();
+  let latestRate = getLatestRate(wrapperAddress, txHash);
+  let amountInUSD = getUSDAmountFromAssetAmount(wrapper.amount, latestRate);
+  wrapper.amountInUSD = amountInUSD;
+  wrapper.save();
+}
+
+function handleBurned(
+  account: Address,
+  principal: BigDecimal,
+  fee: BigDecimal,
+  amountIn: number,
+  transaction: ethereum.Transaction,
+): void {
+  let context = dataSource.context();
+  let wrapperAddress = context.getString('wrapperAddress');
+  let wrapper = Wrapper.load(wrapperAddress);
+  wrapper.amount -= amountIn;
+
+  let txHash = transaction.hash.toHex();
+  let latestRate = getLatestRate(wrapperAddress, txHash);
+  let amountInUSD = getUSDAmountFromAssetAmount(wrapper.amount, latestRate);
+  wrapper.amountInUSD = amountInUSD;
+  wrapper.save();
+}
+
+function handleWrapperMaxTokenAmountUpdated(wrapperAddress: Address, maxTokenAmount: BigDecimal): void {
+  let wrapper = Wrapper.load(wrapperAddress);
+  wrapper.maxAmount = maxTokenAmount;
+  wrapper.save();
+}
+
+function handleEtherWrapperMaxETHUpdated(maxETH: BigDecimal): void {
+  let addressResolverAddress = Address.fromHexString(
+    contracts.get('addressresolver-' + dataSource.network()),
+  ) as Address;
+  let resolver = AddressResolver.bind(addressResolverAddress);
+  let etherWrapperAddress = resolver.try_getAddress(strToBytes('EtherWrapper', 32));
+  if (etherWrapperAddress.reverted) {
+    return;
+  }
+
+  let wrapper = Wrapper.load(etherWrapperAddress.value);
+  wrapper.maxAmount = maxETH;
+  wrapper.save();
+}
---
+++ subgraphs/wrapper.js
@@ -0,0 +1,151 @@
+const { clone, merge } = require('lodash');
+const { getContractDeployments, getCurrentNetwork } = require('./utils/network');
+const latestRates = require('./fragments/latest-rates');
+const manifest = clone(latestRates.dataSources);
+
+getContractDeployments('WrapperFactory').forEach((a, i) => {
+  manifest.push({
+    kind: 'ethereum/contract',
+    name: `wrapperFactory_${i}`,
+    network: getCurrentNetwork(),
+    source: {
+      address: a.address,
+      startBlock: a.startBlock,
+      abi: 'WrapperFactory',
+    },
+    mapping: {
+      kind: 'ethereum/events',
+      apiVersion: '0.0.5',
+      language: 'wasm/assemblyscript',
+      file: '../src/wrapper.ts',
+      entities: [],
+      abis: [
+        {
+          name: 'WrapperFactory',
+          file: '../abis/WrapperFactory.json',
+        },
+      ],
+      eventHandlers: [
+        {
+          event: 'WrapperCreated(indexed address token, bytes32 indexed currencyKey, address wrapperAddress)',
+          handler: 'handleWrapperCreated',
+        },
+      ],
+    },
+  });
+});
+
+const wrapperTemplate = {
+  kind: 'ethereum/contract',
+  name: 'WrapperTemplate',
+  network: getCurrentNetwork(),
+  source: {
+    abi: 'Wrapper',
+  },
+  mapping: {
+    kind: 'ethereum/events',
+    apiVersion: '0.0.5',
+    language: 'wasm/assemblyscript',
+    file: '../src/wrapper.ts',
+    entities: ['Wrapper'],
+    abis: [
+      {
+        name: 'Wrapper',
+        file: '../abis/Wrapper.json',
+      },
+    ],
+    eventHandlers: [
+      {
+        event: 'Minted(indexed address,uint256,uint256,uint256)',
+        handler: 'handleMinted',
+      },
+      {
+        event: 'Burned(indexed address,uint256,uint256,uint256)',
+        handler: 'handleBurned',
+      },
+    ],
+  },
+};
+
+getContractDeployments('EtherWrapper').forEach((a, i) => {
+  manifest.push({
+    kind: 'ethereum/contract',
+    name: `etherWrapper_${i}`,
+    network: getCurrentNetwork(),
+    source: {
+      address: a.address,
+      startBlock: a.startBlock,
+      abi: 'EtherWrapper',
+    },
+    mapping: {
+      kind: 'ethereum/events',
+      apiVersion: '0.0.5',
+      language: 'wasm/assemblyscript',
+      file: '../src/wrapper.ts',
+      entities: ['Wrapper'],
+      abis: [
+        {
+          name: 'EtherWrapper',
+          file: '../abis/EtherWrapper.json',
+        },
+      ],
+      eventHandlers: [
+        {
+          event: 'Minted(indexed address,uint256,uint256,uint256)',
+          handler: 'handleMinted',
+        },
+        {
+          event: 'Burned(indexed address,uint256,uint256,uint256)',
+          handler: 'handleBurned',
+        },
+      ],
+    },
+  });
+});
+
+getContractDeployments('SystemSettings').forEach((a, i) => {
+  manifest.push({
+    kind: 'ethereum/contract',
+    name: `systemSettings_${i}`,
+    network: getCurrentNetwork(),
+    source: {
+      address: a.address,
+      startBlock: a.startBlock,
+      abi: 'SystemSettings',
+    },
+    mapping: {
+      kind: 'ethereum/events',
+      apiVersion: '0.0.5',
+      language: 'wasm/assemblyscript',
+      file: '../src/wrapper.ts',
+      entities: [],
+      abis: [
+        {
+          name: 'SystemSettings',
+          file: '../abis/SystemSettings.json',
+        },
+      ],
+      eventHandlers: [
+        {
+          event: 'WrapperMaxTokenAmountUpdated(address,uint256)',
+          handler: 'handleWrapperMaxTokenAmountUpdated',
+        },
+        {
+          event: 'EtherWrapperMaxETHUpdated(uint256)',
+          handler: 'handleEtherWrapperMaxETHUpdated',
+        },
+      ],
+    },
+  });
+});
+
+module.exports = {
+  specVersion: '0.0.2',
+  description: 'Synthetix Wrapper API',
+  repository: 'https://github.com/Synthetixio/synthetix-subgraph',
+  schema: {
+    file: './wrapper.graphql',
+  },
+  dataSources: manifest,
+  templates: [wrapperTemplate],
+};
