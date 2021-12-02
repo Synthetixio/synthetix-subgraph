36acfdfa2c6c2c94a809611edb4e2dfc89d09c57
Noah Litvin <noah.litvin@gmail.com>
2021-12-02 16:43:59 -0500

    nit

--- src/fragments/latest-rates.ts
+++ src/fragments/latest-rates.ts
@@ -21,7 +21,7 @@ import {
   SynthAggregator,
   InverseAggregator,
 } from '../../generated/subgraphs/latest-rates/templates';
-import { LatestRate, InversePricingInfo, RateUpdate } from '../../generated/subgraphs/latest-rates/schema';
+import { LatestRate, InversePricingInfo, RateUpdate, DailyCandle } from '../../generated/subgraphs/latest-rates/schema';
 
 import {
   BigDecimal,
@@ -33,7 +33,7 @@ import {
   ethereum,
   Bytes,
 } from '@graphprotocol/graph-ts';
-import { strToBytes, toDecimal, ZERO, ZERO_ADDRESS } from '../lib/helpers';
+import { strToBytes, toDecimal, ZERO, ZERO_ADDRESS, toInt } from '../lib/helpers';
 import { ProxyERC20 } from '../../generated/subgraphs/latest-rates/ChainlinkMultisig/ProxyERC20';
 import { Synthetix } from '../../generated/subgraphs/latest-rates/ChainlinkMultisig/Synthetix';
 import { ExecutionSuccess } from '../../generated/subgraphs/latest-rates/ChainlinkMultisig/GnosisSafe';
@@ -69,6 +69,32 @@ export function addLatestRateFromDecimal(
   rateUpdate.block = event.block.number;
   rateUpdate.timestamp = event.block.timestamp;
   rateUpdate.save();
+
+  updateDailyCandle(event.block.timestamp, synth, toInt(rate));
+}
+
+export function updateDailyCandle(timestamp: BigInt, synth: string, rate: BigInt): void {
+  let dayID = timestamp.toI32() / 86400;
+  let newCandle = DailyCandle.load(dayID.toString() + '-' + synth);
+  if (newCandle == null) {
+    newCandle = new DailyCandle(dayID.toString() + '-' + synth);
+    newCandle.synth = synth;
+    newCandle.open = rate;
+    newCandle.high = rate;
+    newCandle.low = rate;
+    newCandle.close = rate;
+    newCandle.timestamp = timestamp;
+    newCandle.save();
+    return;
+  }
+  if (newCandle.low > rate) {
+    newCandle.low = rate;
+  }
+  if (newCandle.high < rate) {
+    newCandle.high = rate;
+  }
+  newCandle.close = rate;
+  newCandle.save();
 }
 
 export function addDollar(dollarID: string): void {
