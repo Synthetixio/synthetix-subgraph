758f58aa6d24894475cc06ec12dba6c4ac221230
Noah Litvin <noah.litvin@gmail.com>
2022-01-13 18:53:52 -0500

    fix

--- subgraphs/loans.js
+++ subgraphs/loans.js
@@ -161,10 +161,6 @@ getContractDeployments('CollateralEth').forEach((a, i) => {
           handler: 'handleLoanPartiallyLiquidated',
         },
         {
-          event: 'LoanRepaymentMade(indexed address,indexed address,uint256,uint256,uint256)',
-          handler: 'handleRepaymentMade',
-        },
-        {
           event: 'CollateralDeposited(indexed address,uint256,uint256,uint256)',
           handler: 'handleCollateralDeposited',
         },
