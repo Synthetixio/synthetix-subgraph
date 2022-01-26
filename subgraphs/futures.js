f3562a223681361c1bd4e00ba0551aee6e27245b
Kevin Meraz <kevinemeraz@gmail.com>
2022-01-26 14:38:16 -0500

    add hard-coded contract addresses for proxy market contracts

--- subgraphs/futures.js
+++ subgraphs/futures.js
@@ -36,44 +36,6 @@ manifest.push({
   },
 });
 
-// manifest.push({
-//   kind: 'ethereum/contract',
-//   name: 'futures_FuturesMarketManager',
-//   network: getCurrentNetwork(),
-//   source: {
-//     address: '0xF11112E2619EDD0192fbE51655115E9f041C11b3',
-//     startBlock: 0,
-//     abi: 'FuturesMarketManager',
-//   },
-//   mapping: {
-//     kind: 'ethereum/events',
-//     apiVersion: '0.0.5',
-//     language: 'wasm/assemblyscript',
-//     file: '../src/futures.ts',
-//     entities: ['FuturesMarket'],
-//     abis: [
-//       {
-//         name: 'FuturesMarket',
-//         file: '../abis/FuturesMarket.json',
-//       },
-//       {
-//         name: 'FuturesMarketManager',
-//         file: '../abis/FuturesMarketManager.json',
-//       },
-//     ],
-//     eventHandlers: [
-//       {
-//         event: 'MarketAdded(address,indexed bytes32)',
-//         handler: 'handleMarketAdded',
-//       },
-//       {
-//         event: 'MarketRemoved(address,indexed bytes32)',
-//         handler: 'handleMarketRemoved',
-//       },
-//     ],
-//   },
-// });
-
 manifest.push({
   kind: 'ethereum/contract',
   name: 'futures_FuturesMarketManager',
@@ -107,6 +69,99 @@ manifest.push({
       {
         event: 'MarketRemoved(address,indexed bytes32)',
         handler: 'handleMarketRemoved',
+      },
+    ],
+  },
+});
+
+// ProxyFuturesMarketBTC
+manifest.push({
+  kind: 'ethereum/contract',
+  name: 'futures_FuturesMarket_BTC',
+  network: getCurrentNetwork(),
+  source: {
+    address: '0xce034450a5C59a05abf4d8b4141f839A13fE71BA',
+    startBlock: 0,
+    abi: 'FuturesMarket',
+  },
+  mapping: {
+    kind: 'ethereum/events',
+    apiVersion: '0.0.5',
+    language: 'wasm/assemblyscript',
+    file: '../src/futures.ts',
+    entities: ['FuturesMarket', 'FuturesPosition', 'FuturesTrade'],
+    abis: [
+      {
+        name: 'FuturesMarket',
+        file: '../abis/FuturesMarket.json',
+      },
+    ],
+    eventHandlers: [
+      {
+        event: 'MarginTransferred(indexed address,int256)',
+        handler: 'handleMarginTransferredBTC',
+      },
+    ],
+  },
+});
+
+// ProxyFuturesMarketETH
+manifest.push({
+  kind: 'ethereum/contract',
+  name: 'futures_FuturesMarket_ETH',
+  network: getCurrentNetwork(),
+  source: {
+    address: '0xf2aCEbE185B0BAB0d0d94B7347b92baEE0996288',
+    startBlock: 0,
+    abi: 'FuturesMarket',
+  },
+  mapping: {
+    kind: 'ethereum/events',
+    apiVersion: '0.0.5',
+    language: 'wasm/assemblyscript',
+    file: '../src/futures.ts',
+    entities: ['FuturesMarket', 'FuturesPosition', 'FuturesTrade'],
+    abis: [
+      {
+        name: 'FuturesMarket',
+        file: '../abis/FuturesMarket.json',
+      },
+    ],
+    eventHandlers: [
+      {
+        event: 'MarginTransferred(indexed address,int256)',
+        handler: 'handleMarginTransferredETH',
+      },
+    ],
+  },
+});
+
+// ProxyFuturesMarketLINK
+manifest.push({
+  kind: 'ethereum/contract',
+  name: 'futures_FuturesMarket_LINK',
+  network: getCurrentNetwork(),
+  source: {
+    address: '0x5c58eC5eb707197362929Ee6ECd160b69881f9d1',
+    startBlock: 0,
+    abi: 'FuturesMarket',
+  },
+  mapping: {
+    kind: 'ethereum/events',
+    apiVersion: '0.0.5',
+    language: 'wasm/assemblyscript',
+    file: '../src/futures.ts',
+    entities: ['FuturesMarket', 'FuturesPosition', 'FuturesTrade'],
+    abis: [
+      {
+        name: 'FuturesMarket',
+        file: '../abis/FuturesMarket.json',
+      },
+    ],
+    eventHandlers: [
+      {
+        event: 'MarginTransferred(indexed address,int256)',
+        handler: 'handleMarginTransferredLINK',
       },
     ],
   },
