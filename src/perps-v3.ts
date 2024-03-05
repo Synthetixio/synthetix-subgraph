e44fc6601b25401a3a7897ec29e47064b2e11442
Yangjun Wang <yangjun.wang90@gmail.com>
2024-03-05 15:32:30 +0200

    add account filed to type PositionLiquidation

--- src/perps-v3.ts
+++ src/perps-v3.ts
@@ -96,6 +96,7 @@ export function handlePositionLiquidated(event: PositionLiquidatedEvent): void {
   liquidation.marketId = event.params.marketId;
   liquidation.amount = event.params.amountLiquidated;
   liquidation.accountId = event.params.accountId;
+  liquidation.account = event.params.accountId.toString();
   liquidation.notionalAmount = estiamtedNotionalSize;
   liquidation.estimatedPrice = market.lastPrice;
   liquidation.timestamp = event.block.timestamp;
